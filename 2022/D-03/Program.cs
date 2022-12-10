17b73ac847f333c90f8e6dc54eee9f0f21337f23
Amos <amos2580@hotmail.co.uk>
2022-12-10 22:42:04 +0000

    Day 3 - Part 2 Solution

--- 2022/D-03/Program.cs
+++ 2022/D-03/Program.cs
@@ -11,21 +11,45 @@ public static class Program
     public static void Main()
     {
         var fileLines = File.ReadAllLines("C:\\TEMP\\input.txt");
-        var tally = 0;
 
+        var elfCollection = new List<List<string>>();
+        var elfGroup = new List<string>();
+        elfCollection.Add(elfGroup);
+
+        var tallyPartOne = 0;
+        var tallyPartTwo = 0;
+
+        // Sort into 3
         foreach (var line in fileLines)
         {
-            var middle = line.Length / 2;
-            var partOne = line.Substring(0, middle);
-            var partTwo = line.Substring(middle, middle);
-            var commonCharacters = partOne.Intersect(partTwo);
-
-            foreach (var character in commonCharacters)
+            if (elfGroup.Count != 3)
             {
-                tally += CharacterToPriority(character);
+                elfGroup.Add(line);
+            }
+            else
+            {
+                elfGroup = new List<string> {line};
+                elfCollection.Add(elfGroup);
             }
         }
 
-        Console.WriteLine(tally);
+        foreach (var elfG in elfCollection)
+        {
+            tallyPartOne += (from elf in elfG
+                let middle = elf.Length / 2
+                let partOne = elf[..middle]
+                let partTwo = elf.Substring(middle, middle)
+                select partOne.Intersect(partTwo)
+                into commonCharacters
+                select commonCharacters.Sum(CharacterToPriority)).Sum();
+
+            // This would fail if the group size was less than 3. In the AoC example, it is 300 strings, divisible by 3.
+            if (elfG.Count != 3) throw new Exception("There is a group with less than 3 strings");
+            var groupPriority = elfG[0].Intersect(elfG[1]).Intersect(elfG[2]).FirstOrDefault();
+            
+            tallyPartTwo += CharacterToPriority(groupPriority);
+        }
+
+        Console.WriteLine($"Part One: {tallyPartOne} - Part Two: {tallyPartTwo}");
     }
 }
